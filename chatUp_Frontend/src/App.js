6d2d98e487ffe80c958c35212031b88d01462728
zed <detrinity27@gmail.com>
2023-04-08 10:36:30 -0700

    registration form ui completed

--- chatUp_Frontend/src/App.js
+++ chatUp_Frontend/src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </div>
